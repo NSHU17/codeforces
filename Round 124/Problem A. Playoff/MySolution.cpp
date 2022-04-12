421d1e242efb71b88f818769394514e32c4f7c89
N S <32284725+NSHU17@users.noreply.github.com>
2022-04-12 16:38:18 -0500

    Create MySolution.cpp

---
+++ Round 124/Problem A. Playoff/MySolution.cpp
@@ -0,0 +1,25 @@
+By NS17, contest: Educational Codeforces Round 124 (Rated for Div. 2), problem: (A) Playoff, Accepted, #, Copy
+
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main(){
+     
+      ios::sync_with_stdio(false);
+      cin.tie(NULL);
+     
+      int t;
+      cin >> t;
+     
+      while (t--){
+        int n;
+        cin >> n;
+     
+        long long y = (pow(2, n) - 1);
+     
+        cout << y << endl;
+      }
+    }
+
+→Judgement Protocol
+Test: #1, time: 15 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK 
